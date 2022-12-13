f377ac58d2c7fa7df31775fdfbc2573529fa00dc
Checkmate128 <checkmate108@icloud.com>
2022-12-12 23:50:52 -0500

    Updated UINumberFieldComponent

--- src/tungsten_ui/ui/component/UINumberFieldComponent.java
+++ src/tungsten_ui/ui/component/UINumberFieldComponent.java
@@ -11,18 +11,26 @@ public class UINumberFieldComponent extends UIComponent {
     private UIRectangleComponent background;
     private UITextComponent numberDisplay;
 
-    private int number = 0;
+    private int number;
+    private int min;
+    private int max;
 
-    public UINumberFieldComponent(int x, int y, int width, int height) {
+    public UINumberFieldComponent(int x, int y, int width, int height, int value, int min, int max) {
         super(x, y, width, height);
+        this.min = min;
+        this.max = max;
+        this.number = value;
         background = new UIRectangleComponent(0, 0, width, height, Color.WHITE);
         increaseButton = new UIButtonComponent(0, 0, width, height / 3, "");
         decreaseButton = new UIButtonComponent(0, 2 * (height / 3), width, height / 3,"");
         numberDisplay = new UITextComponent(0, 0, width, height, 20, "0");
     }
 
-    public UINumberFieldComponent(int x, int y) {
+    public UINumberFieldComponent(int x, int y, int value, int min, int max) {
         super(x, y);
+        this.min = min;
+        this.max = max;
+        this.number = value;
         background = new UIRectangleComponent(0, 0, width, height, Color.WHITE);
         increaseButton = new UIButtonComponent(0, 0, width, height / 3, "");
         decreaseButton = new UIButtonComponent(0, 2 * (height / 3), width, height / 3,"");
@@ -36,14 +44,21 @@ public class UINumberFieldComponent extends UIComponent {
         increaseButton.render(g, offsetX + x, offsetY + y);
         decreaseButton.render(g, offsetX + x, offsetY + y);
         numberDisplay.render(g, offsetX + x, offsetY + y);
+        g.setColor(Color.WHITE);
+        g.fillPolygon(new Polygon(new int[]{increaseButton.x + x + offsetX + 2, increaseButton.x + x + offsetX + increaseButton.width / 2, increaseButton.x + x + offsetX + increaseButton.width - 2}, new int[]{increaseButton.y + increaseButton.height + y + offsetY - 2, increaseButton.y + y + offsetY + 2, increaseButton.y + increaseButton.height + y + offsetY}, 3));
+        g.fillPolygon(new Polygon(new int[]{decreaseButton.x + x + offsetX + 2, decreaseButton.x + x + offsetX + decreaseButton.width / 2, decreaseButton.x + x + offsetX + decreaseButton.width - 2}, new int[]{decreaseButton.y + decreaseButton.height + y + offsetY - 2, decreaseButton.y + y + offsetY + 2, decreaseButton.y + decreaseButton.height + y + offsetY}, 3));
 
         if(MouseInput.x > increaseButton.x + x + offsetX && MouseInput.x < increaseButton.x + x + offsetX + increaseButton.width && MouseInput.y - 26 > increaseButton.y + y + offsetY && MouseInput.y - 26 < increaseButton.y + y + offsetY + increaseButton.height && MouseInput.leftClick && ScreenManager.isReadyForAction()) {
-            number++;
+            if(number < max) {
+                number++;
+            }
             ScreenManager.resetActionDelay();
         }
 
         if(MouseInput.x > decreaseButton.x + x + offsetX && MouseInput.x < decreaseButton.x + x + offsetX + decreaseButton.width && MouseInput.y - 26 > decreaseButton.y + y + offsetY && MouseInput.y - 26 < decreaseButton.y + y + offsetY + decreaseButton.height && MouseInput.leftClick && ScreenManager.isReadyForAction()) {
-            number--;
+            if(number > min) {
+                number--;
+            }
             ScreenManager.resetActionDelay();
         }
     }
--- src/tungsten_ui/ui/component/UIStudentComponent.java
+++ src/tungsten_ui/ui/component/UIStudentComponent.java
@@ -14,7 +14,7 @@ public class UIStudentComponent extends UIComponent {
         this.student = s;
         firstNameField = new UITextFieldComponent(40, 0, 200, 60, 20);
         lastNameField = new UITextFieldComponent(220, 0, 200, 60, 20);
-        gradeNumberField = new UINumberFieldComponent(460, 0, 60, 60);
+        gradeNumberField = new UINumberFieldComponent(460, 0, 60, 60, 9, 12, 9);
         deleteButton = new UIButtonComponent(540, 0, 60, 60, "X");
     }
 }
